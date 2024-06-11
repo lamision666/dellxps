6f47adc76e6cdd1ee10c68e18709c28cf957d462
lamision666 <elmisionero1998@outlook.com>
2024-06-11 20:27:08 +0000

    Add files via upload
    completando

--- optimizado.sh
+++ optimizado.sh
@@ -170,26 +170,97 @@ sgdisk -a 2048 -o /dev/sda
 
 # Crear particiones en función del modo de arranque
 if [ "$bootmode" = "uefi" ]; then
-    sgdisk -n 1:0:+300M -t 1:ef00 -c 1:"UEFI" /dev/sda # Partición EFI
+    sgdisk -n 1:0:+250M -t 1:ef00 -c 1:"BOOT" /dev/sda # Partición EFI/BOOT
     sgdisk -n 2:0:+5G -t 2:8200 -c 2:"SWAP" /dev/sda # Partición SWAP
-    sgdisk -n 3:0:+250M -t 3:8300 -c 3:"ROOT" /dev/sda # Partición ROOT
-    sgdisk -n 4:0:+50G -t 4:0700 -c 4:"SHARED" /dev/sda # Partición para compartir archivos con Windows
-    sgdisk -n 5:0:0 -t 5:8300 -c 5:"HOME" /dev/sda # Partición del usuario
+    sgdisk -n 3:0:+50G -t 3:0700 -c 3:"SHARED" /dev/sda # Partición para compartir archivos con Windows
+    sgdisk -n 4:0:+100G -t 4:8300 -c 4:"HOME" /dev/sda # Partición del usuario
+    sgdisk -n 5:0:0 -t 5:8300 -c 5:"ROOT" /dev/sda # Partición ROOT
 else
     sgdisk -n 1:0:+1M -t 1:ef02 -c 1:"BIOS" /dev/sda # Partición BIOS
     sgdisk -n 2:0:+5G -t 2:8200 -c 2:"SWAP" /dev/sda # Partición SWAP
-    sgdisk -n 3:0:+250M -t 3:8300 -c 3:"ROOT" /dev/sda # Partición ROOT
-    sgdisk -n 4:0:+50G -t 4:0700 -c 4:"SHARED" /dev/sda # Partición para compartir archivos con Windows
-    sgdisk -n 5:0:0 -t 5:8300 -c 5:"HOME" /dev/sda # Partición del usuario
+    sgdisk -n 3:0:+50G -t 3:0700 -c 3:"SHARED" /dev/sda # Partición para compartir archivos con Windows
+    sgdisk -n 4:0:+100G -t 4:8300 -c 4:"HOME" /dev/sda # Partición del usuario
+    sgdisk -n 5:0:0 -t 5:8300 -c 5:"ROOT" /dev/sda # Partición ROOT
 fi
 
 # Formatear las particiones
 mkfs.fat -F32 /dev/sda1
 mkswap /dev/sda2
 swapon /dev/sda2
-mkfs.ext4 /dev/sda3
-mkfs.ntfs -f /dev/sda4
+mkfs.ntfs -f /dev/sda3
+mkfs.ext4 /dev/sda4
 mkfs.ext4 /dev/sda5
 
 # Montar las particiones
+mount /dev/sda5 /mnt
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+mkdir /mnt/home
+mount /dev/sda4 /mnt/home
 
+# Instalar el sistema base
+logo "Instalando el sistema base"
+pacstrap /mnt base base-devel "${kernel}" "${kernel}-firmware" vim sudo
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Configuración básica del sistema
+logo "Configuración básica del sistema"
+echo "$HNAME" > /mnt/etc/hostname
+echo "127.0.0.1 localhost" >> /mnt/etc/hosts
+echo "::1       localhost" >> /mnt/etc/hosts
+echo "127.0.1.1 $HNAME.localdomain $HNAME" >> /mnt/etc/hosts
+
+# Configurar zona horaria
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/"$tzselection" /etc/localtime
+arch-chroot /mnt hwclock --systohc
+
+# Configurar idioma
+echo "LANG=${idiomains}.UTF-8" > /mnt/etc/locale.conf
+echo "LC_COLLATE=C" >> /mnt/etc/locale.conf
+arch-chroot /mnt locale-gen
+
+# Configurar teclado
+echo "KEYMAP=${setkmap}" > /mnt/etc/vconsole.conf
+arch-chroot /mnt localectl set-keymap ${setkmap}
+arch-chroot /mnt localectl set-x11-keymap ${x11keymap}
+
+# Configurar red
+arch-chroot /mnt systemctl enable "${esys}"
+
+# Configurar usuario
+arch-chroot /mnt useradd -m -G wheel -s /bin/bash "$USR"
+echo "$USR:$PASSWD" | arch-chroot /mnt chpasswd
+echo "root:$PASSWDR" | arch-chroot /mnt chpasswd
+sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /mnt/etc/sudoers
+
+# Instalar GRUB
+if [ "$bootmode" = "uefi" ]; then
+    arch-chroot /mnt pacman -Sy --noconfirm grub efibootmgr os-prober
+    arch-chroot /mnt mkdir /boot/EFI
+    arch-chroot /mnt mount /dev/sda1 /boot/EFI
+    arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=GRUB
+else
+    arch-chroot /mnt pacman -Sy --noconfirm grub
+    arch-chroot /mnt grub-install --target=i386-pc /dev/sda
+fi
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+
+# Instalar entorno de escritorio y aplicaciones
+logo "Instalando entorno de escritorio y aplicaciones"
+arch-chroot /mnt pacman -Sy --noconfirm xorg-server xorg-xinit mesa xf86-video-intel "${audiopack}" "${DE}" "${DM}" neofetch
+arch-chroot /mnt systemctl enable "${SDM}"
+
+# Clonar AUR Helper y paquetes
+arch-chroot /mnt pacman -Sy --noconfirm git base-devel
+arch-chroot /mnt git clone https://aur.archlinux.org/yay.git /home/$USR/yay
+arch-chroot /mnt chown -R $USR:$USR /home/$USR/yay
+arch-chroot /mnt sudo -u $USR bash -c "cd /home/$USR/yay && makepkg -si --noconfirm"
+arch-chroot /mnt sudo -u $USR yay -S --noconfirm ${aurbspwm}
+
+# Configuración de red (copia de iwctl)
+logo "Copiando configuración de red de iwctl"
+cp /var/lib/iwd/*.psk /mnt/var/lib/iwd/
+
+# Finalizar
+logo "Instalación completada"
+echo "Puedes reiniciar el sistema."
